823610b09743ce202f0f7a95b0c839d63adbabb5
mariaaxime <mariaximenaam@gmail.com>
2025-12-05 10:08:41 +1100

    Fix Day 3

--- lib/day_three/part_one.rb
+++ lib/day_three/part_one.rb
@@ -1,24 +1,17 @@
 class PartOne
   class << self
     def inputs
-      # File.readlines('lib/day_three/example_input.txt', chomp: true)
-      File.readlines('lib/day_three/input.txt', chomp: true)
+      File.readlines('lib/day_three/example_input.txt', chomp: true)
+      # File.readlines('lib/day_three/input.txt', chomp: true)
     end
 
     def run
       result = []
       inputs.each do |input|
         bank = input.split("").map(&:to_i)
-        first = bank.max
+        first = bank[...-1].max
         index = bank.find_index(first)
-
-        if index == bank.size - 1
-          first = bank.reject {|x| x == first }.max
-          index = bank.find_index(first)
-        end
-
         second = bank[index + 1..].max
-
         max = "#{first}#{second}".to_i
 
         result << max
--- spec/day_three/part_one_spec.rb
+++ spec/day_three/part_one_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/day_three/part_one'
 
 RSpec.describe PartOne do
   specify do
-    expect(PartOne.run).to eq(17430)
-    # expect(PartOne.run).to eq(357) # example
+    # expect(PartOne.run).to eq(17430)
+    expect(PartOne.run).to eq(357) # example
   end
 end
